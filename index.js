7c6d623ea1bcb268e987a507a4307037562d7e66
TBrush123 <dayren.ermekbaev@gmail.com>
2024-06-28 12:31:20 +0500

    Made user schema

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const app = require("./app");
 const db = require("./config/db")
 const dotenv = require("dotenv").config()
 
-
 const port = process.env.PORT;
 
 app.use("/api/contacts", require("./routes/routes"));
