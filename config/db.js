2db1bb81519551e552dacd50fa195d2e78755202
TBrush123 <dayren.ermekbaev@gmail.com>
2024-06-29 14:39:47 +0500

    Made some changes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,9 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+const dotenv = require("dotenv").config();
 
+const server = process.env.SERVER;
 
-const connection = mongoose.createConnection("mongodb://localhost:27017/newDB").on("open", () => {
+const connection = mongoose.createConnection(server + "/newDB").on("open", () => {
     console.log("Database connected");
 }).on("error", () => {
     console.log("Database connection error");
--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const app = require("./app");
-const db = require("./config/db");
-const UserModel = require("./model/user.model");
 const dotenv = require("dotenv").config();
 
 
